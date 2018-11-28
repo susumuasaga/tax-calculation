15a885bff3d204177b8e854e5b06ca1aec54740d
Susumu Asaga <susumu.asaga@gmail.com>
2018-11-28 17:57:08 -0200

    fix: #1 - Locations component looping when fetching locations.

--- src/locationsRedux.spec.tsx
+++ src/locationsRedux.spec.tsx
@@ -7,55 +7,78 @@ import { reducer } from './reducer';
 let storeCreator: MockStoreCreator<State, ThunkDispatch<State, null, Action>>;
 let store: MockStoreEnhanced<State, ThunkDispatch<State, null, Action>>;
 
-describe('Locations Redux', () => {
+describe('Locations Store', () => {
   beforeAll(async () => {
     storeCreator = reduxMockStore([reduxThunk]);
   });
 
-  beforeEach(async () => {
-    store = storeCreator({
-      locationsCache: { isFetching: false },
-      transactionsCache: { isFetching: false }
+  describe('when cache is not present', () => {
+    beforeEach(async () => {
+      store = storeCreator({
+        locationsCache: { isFetching: false },
+        transactionsCache: { isFetching: false }
+      });
+    });
+
+    it('should fetch locations', async () => {
+      await store.dispatch(fetchLocations());
+      let state = store.getState();
+      const actions = store.getActions() as Action[];
+      state = reducer(state, actions[0]);
+      let cache = state.locationsCache;
+      expect(cache.isFetching)
+        .toBe(true);
+      state = reducer(state, actions[1]);
+      cache = state.locationsCache;
+      expect(cache.isFetching)
+        .toBe(false);
+      expect(cache.locations!.length)
+        .toBeGreaterThan(0);
     });
   });
 
-  it('can fetch locations', async () => {
-    let state = store.getState();
-    await store.dispatch(fetchLocations());
-    const actions = store.getActions() as Action[];
-    state = reducer(state, actions[0]);
-    let cache = state.locationsCache;
-    expect(cache.isFetching)
-      .toBe(true);
-    state = reducer(state, actions[1]);
-    cache = state.locationsCache;
-    expect(cache.isFetching)
-      .toBe(false);
-    expect(cache.locations!.length)
-      .toBeGreaterThan(0);
+  describe('when cache is present', () => {
+    beforeEach(async () => {
+      store = storeCreator({
+        locationsCache: {
+          isFetching: false,
+          locations: [{
+            companyId: '75106750-1ae4-4872-9d9b-562d94ea324f',
+            code: '27227668000203',
+            federalTaxId: '27.227.668/0002-03',
+            taxRegime: 'realProfit',
+            address: {
+              cityCode: 3550308,
+              cityName: 'São Paulo',
+              state: 'SP'
+            }
+          }]
+        },
+        transactionsCache: { isFetching: false }
+      });
+    });
+
+    it('should not fetch locations', async () => {
+      await store.dispatch(fetchLocations());
+      const actions = store.getActions() as Action[];
+      expect(actions.length)
+        .toBe(0);
+    });
   });
 
-  it('does not fetch locations, when already cached', async () => {
-    store = storeCreator({
-      locationsCache: {
-        isFetching: false,
-        locations: [{
-          companyId: '75106750-1ae4-4872-9d9b-562d94ea324f',
-          code: '27227668000203',
-          federalTaxId: '27.227.668/0002-03',
-          taxRegime: 'realProfit',
-          address: {
-            cityCode: 3550308,
-            cityName: 'São Paulo',
-            state: 'SP'
-          }
-        }]
-      },
-      transactionsCache: { isFetching: false }
+  describe('when cache is fetching', () => {
+    beforeEach(async () => {
+      store = storeCreator({
+        locationsCache: { isFetching: true },
+        transactionsCache: { isFetching: false }
+      });
     });
-    await store.dispatch(fetchLocations());
-    const actions = store.getActions() as Action[];
-    expect(actions.length)
-      .toBe(0);
+
+    it('should not fetch locations', async () => {
+      await store.dispatch(fetchLocations());
+      const actions = store.getActions() as Action[];
+      expect(actions.length)
+        .toBe(0);
+    });
   });
 });
