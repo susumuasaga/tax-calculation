f974498f179e1020ca936ecff5e856287565c06d
Susumu Asaga <susumu.asaga@gmail.com>
2018-12-04 16:33:40 -0200

    fix: #1 - Implement changes proposed by Client J.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-sudo chmod -R 777 /home/ec2-user/tax-calculation
-sudo chmod -R 777 /usr/local/bin
-cd /home/ec2-user/tax-calculation
 npm install forever -g
-npm install
+sudo chmod 777 /usr/local/bin/*.*
+stop_server.sh
+sudo chmod -R 777 /home/ec2-user/tax-calculation
+cd /home/ec2-user/tax-calculation
+npm install --production
 npm run test-back
 start_server.sh
