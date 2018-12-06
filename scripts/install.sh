17efe33b70a1a1128b802d5ce8a696aeb616b67f
Susumu Asaga <susumu.asaga@gmail.com>
2018-12-05 23:09:26 -0200

    test: #1 - AWS CodeDeploy scripts/install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+export NVM_DIR="/home/ec2-user/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 export PATH=/usr/local/bin:$PATH
 npm install forever -g
 sudo chmod -R 777 /usr/local/bin
