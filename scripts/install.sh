5d7bab0f94c11b3df3ea44bbc962c3cf61f084ba
Susumu Asaga <susumu.asaga@gmail.com>
2018-12-06 00:03:01 -0200

    fix: #1 AWS CodeDeploy appspec.yml scripts/install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,9 @@ export NVM_DIR="/home/ec2-user/.nvm"
 export PATH=/usr/local/bin:$PATH
 npm install forever -g
 sudo chmod -R 777 /usr/local/bin
+stop_server.sh
 sudo chmod -R 777 /home/ec2-user/tax-calculation
 cd /home/ec2-user/tax-calculation
 npm install --production
 npm run test-back
+start_server.sh
